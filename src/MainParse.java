c678f19a5cd1e7538ccf7aa6347f01fa76210eb4
Swarggg <swarg@inbox.ru>
2022-02-19 12:26:16 +0300

    TODO (try-catch) realized, added some comments

--- src/MainParse.java
+++ src/MainParse.java
@@ -10,84 +10,13 @@ public class MainParse {
 
     static String inputFile = "src\\phoneBookSrc.html";
 
-
-    public static ArrayList getFIO_collection () throws FileNotFoundException {
-
-        String patternFIO = "<tbody><tr>";
-        String patternPost = "00006C";
-        String name;
-        String patronymic;
-        String surname;
-        String fioBufStr;
-        String clearFIO;
-        String post;
-        String department;
-        int telephone;
-        ArrayList<Worker> workerList = new ArrayList<>();
-
-        //TODO write Exeption
-        Scanner scanFF = new Scanner(new FileReader(inputFile));
-        while (scanFF.hasNextLine()) {
-
-
-            if (scanFF.next().equals(patternFIO)) {
-
-                scanFF.nextLine(); //пропускаем пустую строку
-                fioBufStr = scanFF.nextLine();
-                if (fioBufStr.contains("vydelen\">")) {//пропускаем всякие служебные строки, берем только с  "vydelen"
-
-                    Worker worker = new Worker("name", "patronymic", "surname", "post", "department", "tel");
-                    workerList.add(worker);
-                    int indexOfWorker = workerList.indexOf(worker);
-                    System.out.println("workerList.indexOf(worker)="+indexOfWorker);
-
-                    int lio = fioBufStr.lastIndexOf("<");
-                    clearFIO = fioBufStr.subSequence(58, lio).toString();
-                    if (!clearFIO.contains("  ")) {    //этим условием убираем строчки не относящиеся к людям
-                        //System.out.println(clearFIO);
-
-                        Scanner fioParser = new Scanner(clearFIO);
-                        surname=fioParser.next();
-                        name=fioParser.next();
-                        patronymic=fioParser.next();
-
-                        System.out.print(name+" ");
-                        System.out.print(patronymic+" ");
-                        System.out.println(surname);
-
-                        workerList.set(indexOfWorker, new Worker(name, patronymic, surname, worker.getPost(), worker.getDepartment(), worker.getTelephone()));
-
-                        //System.out.println(workerList.get(indexOfWorker).getName()+workerList.get(indexOfWorker).getPost());
-                    }
-
-                    String nextBuf= scanFF.nextLine();
-                    post=nextBuf.subSequence(nextBuf.lastIndexOf(patternPost)+9, nextBuf.lastIndexOf("</span>")).toString();
-
-                    department=nextBuf.subSequence(nextBuf.lastIndexOf("<br>")+4, nextBuf.indexOf("</p>")).toString();
-                    System.out.println(post+" "+department);
-
-                    nextBuf= scanFF.nextLine();
-                    System.out.println(nextBuf);
-
-                }
-
-            }
-
-
-
-        } //close while
-        System.out.println("______________________________________________");
-        System.out.println("Number of elements in collection: "+workerList.size());
-        return workerList;
-    }
-
-    public static ArrayList getFIO_collection2 () throws FileNotFoundException {
+    public static ArrayList getFIO_collection () {
 
         String patternPost = "00006C";
         String name;
         String patronymic;
         String surname;
-        String fioBufStr;
+        String buffStr;
         String clearFIO;
         String post;
         String department;
@@ -95,23 +24,22 @@ public class MainParse {
 
         ArrayList<Worker> workerList = new ArrayList<>();
 
-        Scanner scanFF = new Scanner(new FileReader(inputFile));
-         while (scanFF.hasNextLine()) {
-                fioBufStr = scanFF.nextLine();
 
-                whenEmpty:
-                if (fioBufStr.contains("vydelen\">")) {//пропускаем всякие служебные строки, берем только с  "vydelen"
+        try {
+            Scanner scanFF = new Scanner(new FileReader(inputFile));
 
-                    Worker worker = new Worker("name", "patronymic", "surname", "post", "department", "0");
-                    workerList.add(worker);
-                    int indexOfWorker = workerList.indexOf(worker);
-                    //System.out.println("workerList.indexOf(worker)="+indexOfWorker);
+                while (scanFF.hasNextLine()) {
+                buffStr = scanFF.nextLine();
 
-                    clearFIO = fioBufStr.subSequence(fioBufStr.indexOf("vydelen\">")+9, fioBufStr.lastIndexOf("<")).toString();
+                    whenEmpty:
+                    if (buffStr.contains("vydelen\">")) {//выбираем строки только с  "vydelen", т.к. только в них начинают идти имена
 
-                    if (clearFIO.contains("  ")) {    //этим условием пропускаем строчки не относящиеся к людям
-                        break whenEmpty;
-                    }
+                    //формируем строку только с ФИО
+                    clearFIO = buffStr.subSequence(buffStr.indexOf("vydelen\">")+9, buffStr.lastIndexOf("<")).toString();
+
+                        if (clearFIO.contains("  ")) {    //этим условием пропускаем строчки не относящиеся к людям
+                            break whenEmpty; //goto ))
+                        }
 
                     Scanner fioParser = new Scanner(clearFIO);
                     surname=fioParser.next();
@@ -122,47 +50,38 @@ public class MainParse {
                     //System.out.print(patronymic+" ");
                     //System.out.println(surname);
 
-
-
-                           //System.out.println(workerList.get(indexOfWorker).getName()+workerList.get(indexOfWorker).getPost());
-
-
-                    String nextBuf= scanFF.nextLine();
+                    //берем следующую строку, в ней идут должность и подразделение
+                    String nextBuf=scanFF.nextLine();
                     post=nextBuf.subSequence(nextBuf.lastIndexOf(patternPost)+9, nextBuf.lastIndexOf("</span>")).toString();
-
                     department=nextBuf.subSequence(nextBuf.lastIndexOf("<br>")+4, nextBuf.indexOf("</p>")).toString();
 
-
+                    //берем следующую строку, в ней идет телефон
                     nextBuf= scanFF.nextLine();
-                    if (!nextBuf.contains("Местн:")) {
+
+                    if (!nextBuf.contains("Местн:")) { //исключаем строки, где нет местного телефона
                         telephone=" ";
                     } else {
                         telephone = nextBuf.subSequence(nextBuf.indexOf("Местн:") + 5, nextBuf.indexOf("<br>")).toString();
                     }
-                    //System.out.println(nextBuf);
 
-                    //System.out.println("                                   Tele: "+telephone);
+                    //собираем все данные в экземпляр и добавляем в коллекцию
+                    workerList.add(new Worker(name, patronymic, surname, post, department, telephone));
+                    } //close if "vydelen"
+                } //close while
 
-                    workerList.set(indexOfWorker, new Worker(name, patronymic, surname, post, department, telephone));
-
-                }
-
-
+        }   catch (FileNotFoundException fnfe) {
+            System.out.println("File "+inputFile+" not found. Add file and restart programm.");
+            }
 
 
-
-        } //close while
         System.out.println("______________________________________________");
         System.out.println("Number of elements in collection: "+workerList.size());
         return workerList;
     }
 
+    public static void main(String[] args) {
 
-
-    public static void main(String[] args) throws FileNotFoundException {
-
-        getFIO_collection2();
-
+        getFIO_collection();
 
         }
 
